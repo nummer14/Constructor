47b41a7aa66514d4ebd33ba3060c485322bffa97
numme <nummer14@naver.com>
2025-06-11 09:47:39 +0900

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+    Person p1 = new Person("홍성현", 20);
+    Person p2 = new Person("h662", 60);
+
+    /*int a = 10;
+    a = 10;
+    int a =10;*/
+    }
+}
