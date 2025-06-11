dd927f7c712050dfc293647f9fa8d83cbc086788
numme <nummer14@naver.com>
2025-06-11 10:37:29 +0900

    Third Commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
-class Car {
-    String brand;
-    int year;
-    String model;
-
-    public Car(String brand, int year, String model) {
-        this.brand = brand;
-        this.year = year;
-        this.model = model;
-    }
-
-    public void displayinfo() {
-        System.out.println("Car brand: " + brand + ", Year: " + year + ", Model: " + model);
-
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("KIA", 2025, "K9");
-
-        car.displayinfo();
+        System.out.print("프린트 함수와 ");
+        System.out.println("프린트LN 함수의");
+        System.out.println("차이");
     }
 }
